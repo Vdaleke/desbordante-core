46c7cd564ebc3b6808204b4a9aea15721e9a2eb7
Vdaleke <vdalekesmirnov@gmail.com>
2024-05-20 20:39:29 +0300

    Initialize DynFD model
    Add constructors, make_for() functions, getters in DynFD model classes

--- src/core/algorithms/fd/dynfd/model/dynamic_relation_data.h
+++ src/core/algorithms/fd/dynfd/model/dynamic_relation_data.h
@@ -2,12 +2,30 @@
 
 #include <memory>
 
+#include "compressed_column_data.h"
 #include "model/table/idataset_stream.h"
+#include "model/table/relation_data.h"
 
 namespace model {
 
-class DynamicRelationData {
+class DynamicRelationData : AbstractRelationData<CompressedColumnData> {
+    std::unordered_map<std::string, int> value_dictionary_;
+
+    [[nodiscard]] size_t GetNumRows() const final {
+        if (column_data_.empty()) {
+            return 0;
+        }
+        return column_data_[0].GetNumRows();
+    }
+
     static std::unique_ptr<DynamicRelationData> CreateFrom(model::IDatasetStream& data_stream);
+
+public:
+    explicit DynamicRelationData(std::unique_ptr<RelationalSchema> schema,
+                                 std::vector<ColumnType> column_data,
+                                 std::unordered_map<std::string, int> value_dictionary)
+        : AbstractRelationData(std::move(schema), std::move(column_data)),
+          value_dictionary_(std::move(value_dictionary)) {}
 };
 
 }  // namespace model
