45f9ee9de1942cdf88f2d576a753530e8bdc0e3b
Vdaleke <vdalekesmirnov@gmail.com>
2024-05-26 22:38:12 +0300

    Add FullIntersect to DPLI

--- src/core/algorithms/fd/dynfd/model/dynamic_position_list_index.h
+++ src/core/algorithms/fd/dynfd/model/dynamic_position_list_index.h
@@ -24,15 +24,32 @@ private:
 
 public:
     DynamicPositionListIndex(std::list<Cluster> clusters,
-                             std::unordered_map<int, Cluster*> inverted_index, unsigned int size);
+                             std::unordered_map<int, Cluster*> inverted_index,
+                             std::unordered_map<int, int> hash_index, unsigned int size);
     static std::unique_ptr<DynamicPositionListIndex> CreateFor(std::vector<int> const& data);
 
     unsigned int GetSize() const {
         return size_;
     }
 
-    // std::unique_ptr<DynamicPositionListIndex> Intersect(DynamicPositionListIndex const* that)
-    // const;
+    void Insert(int record_id, int value_id) {
+        hash_index_[record_id] = value_id;
+        if (inverted_index_.find(value_id) == inverted_index_.end()) {
+            clusters_.emplace_back();
+            inverted_index_[value_id] = &clusters_.back();
+        }
+        (*inverted_index_[value_id]).insert(record_id);
+        size_++;
+    }
+
+    void Erase(int record_id) {
+        int value_id = hash_index_[record_id];
+        (*inverted_index_[value_id]).erase(record_id);
+        hash_index_.erase(record_id);
+        size_--;
+    }
+
+    std::unique_ptr<DynamicPositionListIndex> FullIntersect(DynamicPositionListIndex const* that) const;
 
     std::string ToString() const;
 };
